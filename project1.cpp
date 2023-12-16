18e0ceb429f3e0a1593dfe635ce511b524101782
isllamkhalil <118750615+isllam-colon@users.noreply.github.com>
2023-12-16 14:43:45 +0200

    Update project1.cpp

--- project1.cpp
+++ project1.cpp
@@ -2,7 +2,51 @@
 #include <queue>
 #include <deque>
 using namespace std;
+#define size 100
 
+struct student
+{
+    double grade;
+    string name;
+    int id;
+};
+
+
+student Queue[size];
+int front = -1;
+int rear = -1;
+
+void enqueue(student Student)
+{
+    if(rear != size-1)
+    {
+        if(front == -1 && rear == -1)
+        {
+            front++;
+                Queue[++rear]= Student;
+        }
+        else 
+        {
+                Queue[++rear]= Student;
+        }
+    }
+    else
+    {
+        cout<<"The list is full" << endl;
+    }
+}
+void addStudent() {
+    student Student;
+    cout << "Enter student ID: ";
+    cin >> Student.id;
+    cout << "Enter student name: ";
+    cin >> Student.name;
+    cout << "Enter student grade: ";
+    cin >> Student.grade;
+
+    enqueue(Student);
+    cout << "Student added to the queue." << endl;
+}
 int main()
 {
 
