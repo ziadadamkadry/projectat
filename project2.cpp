7f6fa18234fdda66455adf92a0ae7d3d4821d5a8
ziadadamkadry <115738150+ziadadamkadry@users.noreply.github.com>
2023-12-21 10:15:32 +0200

    Update project2.cpp

--- project2.cpp
+++ project2.cpp
@@ -2,66 +2,99 @@
 #include <string>
 using namespace std;
 
-struct Contact
+struct contact
 {
     string name;
     string tel;
-    Contact* left;
-    Contact* right;
+    contact* left;
+    contact* right;
 };
 
-void addEntry(Contact*& root, string& name,  string& tel) 
-{
-    Contact* newContact = new Contact(name, tel);
+void addcontact(contact*& newcontact, string nme, string num){
 
-    if (root == nullptr) 
-    {
-        root = newContact;
-    } 
-    else 
-    {
-        Contact* temp = root;
-        while (true) 
-        {
-            if (name < temp->name) 
-            {
-                if (temp->left == nullptr)
-                {
-                    temp->left = newContact;
-                    break;
-                } 
-                else 
-                {
-                    temp = temp->left;
-                }
-            } 
-            else 
-            {
-                if (temp->right == nullptr)
-                {
-                    temp->right = newContact;
-                    break;
-                } 
-                else
-                {
-                    temp = temp->right;
-                } 
-            }
-        }
+    newcontact = new contact;
+    newcontact->name = nme;
+    newcontact->tel = num;
+    newcontact->left = nullptr;
+    newcontact->right = nullptr;
+}
+
+void adbol(contact*& root, string nme, string num){
+    if(root == NULL){
+    addcontact(root, nme, num);
+    return;
     }
+    if (nme < root->name){
+        return adbol(root->left, nme, num);
+    }
+    else{
+        return adbol(root->right, nme, num);
+    }
+}
 
-    cout << "added successfully." << endl;
+void addtobook(contact*& root){
+    string nme;
+    string num;
+
+    cout<<"enter a contact: ";getline(cin, nme);
+    cout<<"enter it's number: ";getline(cin, num);cout<<endl;
+    cout<<"contact added! ";cout<<endl;
+
+    (adbol(root, nme, num));
+
+
 }
-void addcontact(string& name, string& tel) 
-{
-    cout << "Enter the name: ";
-    getline(cin, name);
-    cout << "Enter the telephone number: ";
-    getline(cin, tel);
+
+string srchbolnum(contact*& root, string nme){
+    if(root->name == nme){
+        string nom;
+        nom = root->tel;
+        return nom;
+    }
+    if (nme < root->name){
+        return srchbolnum(root->left, nme);
+    }
+    else{
+        return srchbolnum(root->right, nme);
+    }
 }
+
+bool srchbol(contact*& root, string nme){
+    if(root == NULL){
+        return false;
+    }
+    if(root->name == nme){
+        return true;
+    }
+    if (nme < root->name){
+        return srchbol(root->left, nme);
+    }
+    else{
+        return srchbol(root->right, nme);
+    }
+}
+
+void search(contact*& root){
+    string nme;
+    cout<<"enter the contact to be found: ";getline(cin, nme);
+    if (srchbol(root, nme)){
+        cout<<nme<<" is found and the number is "<<srchbolnum(root, nme); 
+    }
+    else{
+        cout<<nme<<" is not found";
+    }
+}
+
+
 
 int main()
 {
+    contact* root = nullptr;
+    addtobook(root);
+    addtobook(root);
+    addtobook(root);
+    search(root);
+
     return 0;
 
 }
